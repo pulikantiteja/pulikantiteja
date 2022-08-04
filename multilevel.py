e6940ad6353a5397fa827c27facbd67c9c6c0584
pulikantiteja <110401725+pulikantiteja@users.noreply.github.com>
2022-08-04 11:08:29 +0530

    Add files via upload
    added multilevel_inheritance

---
+++ multilevel.py
@@ -0,0 +1,23 @@
+class operatingSystem:
+    def windows(self):
+        print("windows works samsung")
+        
+
+    def mac(self):
+        print("mac works apple")
+        
+        
+
+    def linux(self):
+        print("linux works linux")
+        
+        
+class ubantu(operatingSystem):
+    def sys(self):
+        print("ubantu works  ubantu")
+ 
+ 
+ child=ubantu()
+ child.mac()
+ child.windows()
+ 
